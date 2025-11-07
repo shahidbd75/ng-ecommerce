c395acb6da8b912e6ee1b9a5f31519579b4c26de
Abdul Baset Shahid <abdul.baset@iqvia.com>
2025-11-07 11:14:22 +0600

    product filter applied in home page

--- src/app/pages/products-grid/products-grid.ts
+++ src/app/pages/products-grid/products-grid.ts
@@ -2,10 +2,13 @@ import { Component, computed, input, signal } from '@angular/core';
 import { Product } from '../../models/product';
 import { ProductsData } from '../../data/products';
 import { ProductCard } from '../../components/product-card/product-card';
-
+import { MatSidenav, MatSidenavContent, MatSidenavContainer } from '@angular/material/sidenav';
+import { MatListItem, MatNavList, MatListItemTitle } from '@angular/material/list';
+import { RouterLink } from "@angular/router";
+import { TitleCasePipe } from '@angular/common';
 @Component({
   selector: 'app-products-grid',
-  imports: [ProductCard],
+  imports: [ProductCard, MatSidenav, MatSidenavContent, MatSidenavContainer, MatNavList, MatListItem, MatListItemTitle, RouterLink, TitleCasePipe],
   templateUrl: './products-grid.html',
   styles: ``,
 })
@@ -14,6 +17,8 @@ export default class ProductsGrid {
 
 
   products = signal<Product[]>(ProductsData);
+
+  categories = signal<string[]>(['all', ...new Set(this.products().map(p => p.category))]);
 
   filteredProduct = computed(() => {
     if(this.category() === 'all') return this.products();
